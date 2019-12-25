fc19e963bd480303e1691b3ff683f53378b26f67
Critter4Dinner <woffordben1992@gmail.com>
2019-12-25 14:20:06 -0500

    added config folder with connection string global variable

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.get('/', (req, res) => res.send("You made it!") )
+
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`))
