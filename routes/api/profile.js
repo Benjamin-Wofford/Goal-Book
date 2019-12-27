6b6d40c67c1227ea78ed7f8cbd9f45a8c2bec036
Critter4Dinner <woffordben1992@gmail.com>
2019-12-27 15:21:48 -0500

    Updated profile route to fetch the profile of the user when profile exists

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,25 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
 
-// The route to the application is GET api/profile
-// The description: Test route
-// Who can access this route. Anyone. No JSW required. 
+// The route to the application is GET api/profile/me
+// The description: Get current user's profile
+// Who can access this route. The current user.
 
-router.get('/', (req, res) => res.send("profile"))
+router.get("/me", auth, async (req, res) => {
+  try {
+    const profile = await await Profile.findOne({
+      user: req.user.id
+    }).populate("user", ["name", "avatar"]);
 
-module.exports = router
+    if (!profile) {
+      return res.status(400).json({ msg: "There is no profile for this user" });
+    }
+  } catch (error) {
+    res.status(500).send("Server error I'm sorry");
+  }
+});
+
+module.exports = router;
