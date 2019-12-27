278da5500f7bf11fc123f4cb7ff635ba608b521e
Critter4Dinner <woffordben1992@gmail.com>
2019-12-26 22:09:50 -0500

    Checks if user exists before creating the user, Get users gravat function added, and encrypts the users password

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 const { check, validationResult } = require("express-validator");
 
+const User = require("../../models/User");
 // The route to the application is POST api/users
 // The description: Sign-up user
 // Who can access this route. Anyone. No JSW required.
@@ -18,14 +21,52 @@ router.post(
       "Please enter a password with 6 or more characters."
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     // Finds the validation errors in this request and wraps them in an object with handy functions
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("users");
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      // See if user exists
+      if (user) {
+        res.status(400).json({ errors: [{ msg: "User already exists" }] });
+      }
+
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: "200",
+        r: "pg",
+        d: "mm"
+      });
+
+      user = new User({
+        name,
+        email,
+        password,
+        avatar,
+        createdAt,
+        goalsCompleted,
+        motto,
+        goals
+      });
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      // Return jsonwebtoken
+
+      res.send("User registered");
+    } catch (error) {
+      console.log(error);
+      res.sendStatus(500).send("Server error");
+    }
   }
 );
 
