bf17e8ffca6c58faa4f78cad15f690b6abb64f6f
Critter4Dinner <woffordben1992@gmail.com>
2019-12-27 12:12:31 -0500

    Set up auth route with the correct data and created middle ware that verified the JWT

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const router = express.Router()
-
+const auth = require('../../middleware/auth')
+const User = require('../../models/User')
 // The route to the application is GET api/auth
 // The description: Test route
-// Who can access this route. Anyone. No JSW required. 
+// Who can access this route. Authorized users. 
 
-router.get('/', (req, res) => res.send("auth"))
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password')
+        res.json(user);
+    } catch (error) {
+        console.error(error.message)
+        res.status(500).send('Server errror')
+    }
+})
 
 module.exports = router
