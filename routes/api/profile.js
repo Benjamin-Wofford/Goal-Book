7f7b78de10f337d552cbbc437910a8ed93b961e6
Critter4Dinner <woffordben1992@gmail.com>
2019-12-27 17:32:12 -0500

    Setup backend route for creating and updating a profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -22,4 +22,64 @@ router.get("/me", auth, async (req, res) => {
   }
 });
 
+// The route to the application is POST api/profile
+// The description: Create or update user profile
+// Who can access this route. PRIVATE.
+
+router.post("/", auth, async (req, res) => {
+  const {
+    location,
+    bio,
+    goals,
+    goalsCompleted,
+    youtube,
+    facebook,
+    twitter,
+    instagram,
+    linkedin
+  } = req.body;
+
+  // Build profile object
+  const profileFields = {};
+  profileFields.user = req.user.id;
+  if (location) profileFields.location = location;
+  if (bio) profileFields.bio = bio;
+  if (goals) profiledFields.goals = goals;
+  if (goalsCompleted) profileFields.goalsCompleted = goalsCompleted;
+
+  // Build social object
+  profileFields.social = {};
+  if (youtube) profileFields.social.youtube = youtube;
+  if (twitter) profileFields.social.twitter = twitter;
+  if (facbeook) profileFields.social.facebook = facebook;
+  if (linkedin) profileFields.social.linkedin = linkedin;
+  if (instagram) profileFields.social.instagram = instagram;
+
+  try {
+    let profile = await Profile.findOne({ user: req.user.id });
+
+    // Update
+    if (profile) {
+      await Profile.findOneAndUpdate(
+        { user: req.user.id },
+        { $set: profileFields },
+        { new: true }
+      );
+
+      return res.json(profile);
+    }
+
+    // Create
+    profile = new Profile(profileFields);
+
+    await profile.save();
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server error");
+  }
+
+  res.send("You made it to the bottom of profile route");
+});
+
 module.exports = router;
