edc0bcf4d04abded470423e0200957a3e5b207ca
Critter4Dinner <woffordben1992@gmail.com>
2019-12-28 18:21:58 -0500

    Added a route that handles the deleting of a comment if the deleter is the original author of route

--- routes/api/goal.js
+++ routes/api/goal.js
@@ -119,55 +119,138 @@ router.delete("/:id", auth, async (req, res) => {
 // The description: Like a goal
 // Who can access this route. Private.
 
-router.put('/like/:id', auth, async (req, res) => {
-try {
-    const goal = await Goal.findById(req.params.id)
+router.put("/like/:id", auth, async (req, res) => {
+  try {
+    const goal = await Goal.findById(req.params.id);
 
     // Check if the post has already been liked
-    if(goal.likes.filter( like => like.user.toString() === req.user.id).length > 0){
-       
-        return res.status(400).json({ msg: "Post already liked" })
+    if (
+      goal.likes.filter(like => like.user.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: "Post already liked" });
     }
 
-    goal.likes.unshift({ user: req.user.id })
+    goal.likes.unshift({ user: req.user.id });
 
-    await goal.save()
+    await goal.save();
 
-    res.json(goal.likes)
-
-} catch (error) {
-  console.error(error.message)
-  res.status(500).send('Server Error in router put')  
-}
-})
+    res.json(goal.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error in router put");
+  }
+});
 
 // The route to the application is PUT api/goal/unlike/:id
 // The description: Unlike a goal
 // Who can access this route. Private.
 
-router.put('/unlike/:id', auth, async (req, res) => {
+router.put("/unlike/:id", auth, async (req, res) => {
+  try {
+    const goal = await Goal.findById(req.params.id);
+
+    // Check if the post has already been liked
+    if (
+      goal.likes.filter(like => like.user.toString() === req.user.id).length ===
+      0
+    ) {
+      return res.status(400).json({ msg: "Goal has not yet been liked" });
+    }
+
+    // Get remove index
+    const removeIndex = goal.likes
+      .map(like => like.user.toString())
+      .indexOf(req.user.id);
+
+    goal.likes.splice(removeIndex, 1);
+
+    await goal.save();
+
+    res.json(goal.likes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Server Error in router put");
+  }
+});
+
+// The route to the application is POST api/goal/comment/:id
+// The description: Create goal comment
+// Who can access this route. Private.
+
+router.post(
+  "/comment/:id",
+  [
+    auth,
+    [
+      check("text", "Text is required")
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await await User.findById(req.user.id).select("-password");
+      const goal = await Goal.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      goal.comments.unshift(newComment);
+
+      await goal.save();
+
+      res.json(goal.comments);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// The route to the application is DELETE api/goal/comment/:id/:comment_id
+// The description: Delete Comment
+// Who can access this route. Private.
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
     try {
         const goal = await Goal.findById(req.params.id)
-    
-        // Check if the post has already been liked
-        if(goal.likes.filter( like => like.user.toString() === req.user.id).length === 0){
-           
-            return res.status(400).json({ msg: "Goal has not yet been liked" })
-        }
-    
-        // Get remove index
-        const removeIndex = goal.likes.map(like => like.user.toString()).indexOf(req.user.id)
 
-        goal.likes.splice(removeIndex, 1)
-    
-        await goal.save()
-    
-        res.json(goal.likes)
-    
+        // Pull out comment
+        const comment = goal.comments.find(comment => comment.id === req.params.comment_id)
+
+        if(!comment){
+            return res.status(404).json({ msg: "Comment does not exists"})
+        }
+
+        // Check if user is the owner of the comment
+        if(comment.user.toString() !== req.user.id){
+            return res.status(401).json({ msg: 'User not authorized'})
+        }
+
+        // Get remove index
+        const removeIndex = goal.comments.map(comment => comment.user.toString()).indexOf(req.user.id)
+        
+        goal.comments.splice(removeIndex, 1)
+
+        await goal.save();
+
+        res.json(goal.comments)
     } catch (error) {
-      console.error(error.message)
-      res.status(500).send('Server Error in router put')  
+        console.error(error.message)
+        res.status(500).send('Server error')
     }
-    })
+})
+
+
+
 
 module.exports = router;
