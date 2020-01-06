d69d65e15291ce5802521c171b7755fd28d6a1ef
Critter4Dinner <woffordben1992@gmail.com>
2020-01-06 09:58:31 -0500

    Working on setting up alert in signup component

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import LoginBox from "./components/layout/LoginBox";
 import NotFound from "./components/layout/NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Signup from "./components/layout/Signup";
-
+import Alert from './components/layout/Alert'
 // Redux
 import { Provider } from "react-redux";
 import store from "./store";
@@ -13,6 +13,7 @@ const App = () => {
   return (
     <Provider store={store}>
       <Router>
+        <Alert/>
         <Switch>
           <Route exact path="/" component={LoginBox} />
           <Route exact path="/Signup" component={Signup} />
--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -1,19 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
+import { Snackbar, IconButton, Button } from "@material-ui/core";
+import CloseIcon from "@material-ui/icons/Close";
+import { makeStyles } from "@material-ui/core/styles";
 
-const Alert = ({ alerts }) => alerts !== null && alerts.length > 0 && alerts.map(alert => (
-    <div key={alert.id}>
-        {alert.msg}
-    </div>
-))
+const useStyles = makeStyles(theme => ({
+  close: {
+    padding: theme.spacing(0.5)
+  }
+}));
 
-Alert.PropTypes = {
-  alerts: PropTypes.array.isRequired
+const Alert = ({ alerts }) => {
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const setOpenAlert = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpen(false);
+  };
+
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "left"
+      }}
+      open={open}
+      autoHideDuration={6000}
+      onClose={handleClose}
+      ContentProps={{
+        "aria-describedby": "message-id"
+      }}
+      message={<span id="message-id">Note archived</span>}
+      action={[
+        <Button key="undo" color="secondary" size="small" onClick={handleClose}>
+          UNDO
+        </Button>,
+        <IconButton
+          key="close"
+          aria-label="close"
+          color="inherit"
+          className={classes.close}
+          onClick={handleClose}
+        >
+          <CloseIcon />
+        </IconButton>
+      ]}
+    />
+  );
 };
 
-const mapStateToProps = state = ({
+// Alert.PropTypes = {
+//   alert: PropTypes.array.isRequired
+// };
+
+const mapStateToProps = state => ({
   alerts: state.alert
 });
 
-export default connect()(Alert);
+
+export default connect(mapStateToProps)(Alert);
