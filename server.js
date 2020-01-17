044dbcb4cf0f94e5001ff48d63869f05b2ae7cdc
Critter4Dinner <woffordben1992@gmail.com>
2020-01-17 18:34:16 -0500

    set up server for deployment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require('path')
 
 const app = express();
 
@@ -11,12 +12,20 @@ app.use(express.json({ extended: false }));
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => res.send("You made it!"));
-
 // Define routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/goal", require("./routes/api/goal"));
+
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production'){
+    // Set static folder
+    app.use(express.static('client/build'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
